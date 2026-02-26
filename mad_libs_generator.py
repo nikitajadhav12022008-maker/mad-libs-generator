1ef2035ddaacd2be5c7561fcff0168aec4c8fc92
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-26 08:10:49 -0800

    Implement basic mad libs generator in Python

--- mad_libs_generator.py
+++ mad_libs_generator.py
@@ -1,5 +1,7 @@
+#python based mad libs generator:
 name = input("enter your name:")
 place = input("enter place:")
 animal = input("enter animal:")
+
 story = f"one day {name} went on {place} and saw a funny {animal}"
 print(story)
