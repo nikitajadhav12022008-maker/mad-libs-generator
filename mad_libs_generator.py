0155f516514c9b6f515e8c7b9449e0115302e4bb
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-15 00:53:16 -0700

    Updated mad_libs_generator.py

--- mad_libs_generator.py
+++ mad_libs_generator.py
@@ -1,7 +1,9 @@
-#python based mad libs generator:
+#Python based mad libs generator:
+#input name,place and animal
 name = input("enter your name:")
 place = input("enter place:")
 animal = input("enter animal:")
-
+#defining story
 story = f"one day {name} went on {place} and saw a funny {animal}"
+#final step printing story
 print(story)
